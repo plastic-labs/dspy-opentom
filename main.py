6772b69893368fffab0a830e441e10ac78dc184d
Jacob Van Meter <jacobvm04@gmail.com>
2024-03-04 12:51:32 -0500

    cot with thought

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import dspy
 from dspy.teleprompt import BootstrapFewShotWithRandomSearch, SignatureOptimizer
 from dspy.evaluate.evaluate import Evaluate
 from cot import CoTSimplifiedBaleen
+from cot_with_thought import CoTWithThoughtSimplifiedBaleen
 from get_data import default_factory, load_dataset
 from collections import defaultdict
 from dotenv import load_dotenv
@@ -52,93 +53,97 @@ def main(dspy_method, dspy_optimizer, download_dataset, question_types, teacher_
         datasets = pickle.load(file)
 
     if dspy_method == "cot":
-        modules = {}
-        # define modules for each question type
-        for question_type in question_types:
-            print(f"TYPE: {question_type}")
-            evaluator = OpenToMEvaluatorDspy(model_name="(training set) complied baleen")
+        module_type = CoTSimplifiedBaleen
+    elif dspy_method == "cot_with_thought":
+        module_type = CoTWithThoughtSimplifiedBaleen
+    else:
+        raise Exception(f"Dspy method '{dspy_method}' is not valid")
 
-            if dspy_optimizer == "bootstrap_fewshot_with_random_search":
-                optimizer = BootstrapFewShotWithRandomSearch(
-                    metric=evaluator.dspy_metric,
-                    num_candidate_programs=25,
-                    num_threads=1,
-                    teacher_settings=dict(lm=teacher_lm),
-                )
-                compiled_baleen = optimizer.compile(
-                    CoTSimplifiedBaleen(), trainset=datasets[question_type]["train"][:train_size]
-                )
-            elif dspy_optimizer == "signature_optimizer":
-                optimizer = SignatureOptimizer(
-                    metric=evaluator.dspy_metric,
-                    breadth=10,
-                    depth=3,
-                    init_temperature=1.4,
-                    verbose=True,
-                    track_stats=True,
-                    prompt_model=teacher_lm,
-                )
-                eval_kwargs = dict(num_threads=1, display_progress=True, display_table=0)
-                compiled_baleen = optimizer.compile(
-                    CoTSimplifiedBaleen(),
-                    devset=datasets[question_type]["train"][:train_size],
-                    eval_kwargs=eval_kwargs,
-                )
-            else:
-                raise Exception(f"Invalid dspy optimizer type: {dspy_optimizer}")
+    modules = {}
+    # define modules for each question type
+    for question_type in question_types:
+        print(f"TYPE: {question_type}")
+        evaluator = OpenToMEvaluatorDspy(model_name="(training set) complied baleen")
 
-            modules[question_type] = compiled_baleen
-            time.sleep(10)
-
-        uncompiled_baleen = CoTSimplifiedBaleen()
-
-        print("Beginning Evaluation")
-        for question_type in question_types:
-            compiled_baleen = modules[question_type]
-
-            # Evaluation Procedure: Calculate the F1 Score for a randomly drawn batch of 50 questions 5 times and average the F1 Scores
-            batch_size = 50
-            num_batches = 5
-
-            assert len(datasets[question_type]["test"]) >= batch_size * num_batches
-            test = datasets[question_type]["test"][: batch_size * num_batches]
-            test_sets = [test[i : i + batch_size] for i in range(num_batches)]
-
-            uncompiled_f1_scores = []
-            compiled_f1_scores = []
-
-            for test in test_sets:
-                # Set up the `evaluate_on_hotpotqa` function.
-                evaluate_on_opentom = Evaluate(devset=test, num_threads=1, display_progress=True, display_table=0)
-
-                uncompiled_baleen_evaluator = OpenToMEvaluatorDspy(model_name="uncompiled_baleen")
-                evaluate_on_opentom(uncompiled_baleen, metric=uncompiled_baleen_evaluator.dspy_metric, display=True)
-                uncompiled_f1_scores.append(uncompiled_baleen_evaluator.f1_score()[question_type]["macro_averaged"])
-
-                compiled_baleen_evaluator = OpenToMEvaluatorDspy(model_name="compiled_baleen")
-                evaluate_on_opentom(compiled_baleen, metric=compiled_baleen_evaluator.dspy_metric, display=True)
-                compiled_f1_scores.append(compiled_baleen_evaluator.f1_score()[question_type]["macro_averaged"])
-
-            # overall f1 scores
-            uncompiled_mean_f1 = np.mean(uncompiled_f1_scores)
-            uncompiled_std_f1 = np.std(uncompiled_f1_scores)
-
-            compiled_mean_f1 = np.mean(compiled_f1_scores)
-            compiled_std_f1 = np.std(compiled_f1_scores)
-
-            run[f"evaluation/{question_type}/uncompiled/mean_macro_averaged_f1"] = uncompiled_mean_f1
-            run[f"evaluation/{question_type}/uncompiled/mean_macro_averaged_f1"] = uncompiled_std_f1
-            run[f"evaluation/{question_type}/compiled/mean_macro_averaged_f1"] = compiled_mean_f1
-            run[f"evaluation/{question_type}/compiled/mean_macro_averaged_f1"] = compiled_std_f1
-
-            print(
-                f"Mean Macro Averaged F1 Scores (± std dev.) - {question_type} - Aggregated from {num_batches} batches of {batch_size} questions"
+        if dspy_optimizer == "bootstrap_fewshot_with_random_search":
+            optimizer = BootstrapFewShotWithRandomSearch(
+                metric=evaluator.dspy_metric,
+                num_candidate_programs=25,
+                num_threads=1,
+                teacher_settings=dict(lm=teacher_lm),
             )
-            print(f"uncompiled: {uncompiled_mean_f1:.3f} ± {uncompiled_std_f1:.3}")
-            print(f"compiled: {compiled_mean_f1:.3} ± {compiled_std_f1:.3}")
+            compiled_baleen = optimizer.compile(module_type(), trainset=datasets[question_type]["train"][:train_size])
+        elif dspy_optimizer == "signature_optimizer":
+            optimizer = SignatureOptimizer(
+                metric=evaluator.dspy_metric,
+                breadth=10,
+                depth=3,
+                init_temperature=1.4,
+                verbose=True,
+                track_stats=True,
+                prompt_model=teacher_lm,
+            )
+            eval_kwargs = dict(num_threads=1, display_progress=True, display_table=0)
+            compiled_baleen = optimizer.compile(
+                module_type(),
+                devset=datasets[question_type]["train"][:train_size],
+                eval_kwargs=eval_kwargs,
+            )
+        else:
+            raise Exception(f"Invalid dspy optimizer type: {dspy_optimizer}")
 
-        dump_state(modules, "cot_modules.pkl")
-        run["cot_modules"].upload("cot_modules.pkl")
+        modules[question_type] = compiled_baleen
+        time.sleep(10)
+
+    uncompiled_baleen = CoTSimplifiedBaleen()  # regular cot is always the uncompiled baseline
+
+    print("Beginning Evaluation")
+    for question_type in question_types:
+        compiled_baleen = modules[question_type]
+
+        # Evaluation Procedure: Calculate the F1 Score for a randomly drawn batch of 50 questions 5 times and average the F1 Scores
+        batch_size = 50
+        num_batches = 5
+
+        assert len(datasets[question_type]["test"]) >= batch_size * num_batches
+        test = datasets[question_type]["test"][: batch_size * num_batches]
+        test_sets = [test[i : i + batch_size] for i in range(num_batches)]
+
+        uncompiled_f1_scores = []
+        compiled_f1_scores = []
+
+        for test in test_sets:
+            # Set up the `evaluate_on_hotpotqa` function.
+            evaluate_on_opentom = Evaluate(devset=test, num_threads=1, display_progress=True, display_table=0)
+
+            uncompiled_baleen_evaluator = OpenToMEvaluatorDspy(model_name="uncompiled_baleen")
+            evaluate_on_opentom(uncompiled_baleen, metric=uncompiled_baleen_evaluator.dspy_metric, display=True)
+            uncompiled_f1_scores.append(uncompiled_baleen_evaluator.f1_score()[question_type]["macro_averaged"])
+
+            compiled_baleen_evaluator = OpenToMEvaluatorDspy(model_name="compiled_baleen")
+            evaluate_on_opentom(compiled_baleen, metric=compiled_baleen_evaluator.dspy_metric, display=True)
+            compiled_f1_scores.append(compiled_baleen_evaluator.f1_score()[question_type]["macro_averaged"])
+
+        # overall f1 scores
+        uncompiled_mean_f1 = np.mean(uncompiled_f1_scores)
+        uncompiled_std_f1 = np.std(uncompiled_f1_scores)
+
+        compiled_mean_f1 = np.mean(compiled_f1_scores)
+        compiled_std_f1 = np.std(compiled_f1_scores)
+
+        run[f"evaluation/{question_type}/uncompiled/mean_macro_averaged_f1"] = uncompiled_mean_f1
+        run[f"evaluation/{question_type}/uncompiled/mean_macro_averaged_f1"] = uncompiled_std_f1
+        run[f"evaluation/{question_type}/compiled/mean_macro_averaged_f1"] = compiled_mean_f1
+        run[f"evaluation/{question_type}/compiled/mean_macro_averaged_f1"] = compiled_std_f1
+
+        print(
+            f"Mean Macro Averaged F1 Scores (± std dev.) - {question_type} - Aggregated from {num_batches} batches of {batch_size} questions"
+        )
+        print(f"uncompiled: {uncompiled_mean_f1:.3f} ± {uncompiled_std_f1:.3}")
+        print(f"compiled: {compiled_mean_f1:.3} ± {compiled_std_f1:.3}")
+
+    dump_state(modules, "cot_modules.pkl")
+    run["cot_modules"].upload("cot_modules.pkl")
 
 
 if __name__ == "__main__":
