b6ef4df88b0bd7bddad34885b51315047441f520
Jacob Van Meter <jacobvm04@gmail.com>
2024-03-04 09:02:11 -0500

    teacher options + qol kwargs

--- cot.py
+++ cot.py
@@ -1,10 +1,4 @@
 import dspy
-from dotenv import load_dotenv
-
-load_dotenv()
-
-turbo = dspy.OpenAI(model='gpt-3.5-turbo', max_tokens=1000)
-dspy.settings.configure(lm=turbo)
 
 
 # DSPy code
@@ -16,12 +10,12 @@ class GenerateAnswer(dspy.Signature):
     answer_choices = dspy.InputField()
     answer = dspy.OutputField(desc="often between 1 and 5 words")
 
+
 class CoTSimplifiedBaleen(dspy.Module):
     def __init__(self):
         super().__init__()
         self.generate_answer = dspy.ChainOfThought(GenerateAnswer)
-    
+
     def forward(self, question, context, answer_choices):
         pred = self.generate_answer(context=context, question=question, answer_choices=answer_choices)
         return dspy.Prediction(context=context, answer=pred.answer)
-    
--- main.py
+++ main.py
@@ -3,12 +3,17 @@
 import pickle
 import time
 import argparse
+from typing import Optional
 from opentom_evaluator import OpenToMEvaluatorDspy
+import dspy
 from dspy.teleprompt import BootstrapFewShotWithRandomSearch, SignatureOptimizer
 from dspy.evaluate.evaluate import Evaluate
 from cot import CoTSimplifiedBaleen
 from get_data import default_factory
 from collections import defaultdict
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 EVAL_QUESTION_TYPES = [
@@ -27,7 +32,7 @@ def dump_state(data, filename):
         pickle.dump(data, file)
 
 
-def main(dspy_method, dspy_optimizer):
+def main(dspy_method, dspy_optimizer, question_types, teacher_lm, train_size):
 
     # read in the datasets pickle object
     with open("datasets.pkl", "rb") as file:
@@ -36,22 +41,35 @@ def main(dspy_method, dspy_optimizer):
     if dspy_method == "cot":
         modules = {}
         # define modules for each question type
-        for question_type in EVAL_QUESTION_TYPES:
+        for question_type in question_types:
             print(f"TYPE: {question_type}")
             evaluator = OpenToMEvaluatorDspy(model_name="(training set) complied baleen")
 
             if dspy_optimizer == "bootstrap_fewshot_with_random_search":
                 optimizer = BootstrapFewShotWithRandomSearch(
-                    metric=evaluator.dspy_metric, num_candidate_programs=25, num_threads=1
+                    metric=evaluator.dspy_metric,
+                    num_candidate_programs=25,
+                    num_threads=1,
+                    teacher_settings=dict(lm=teacher_lm),
                 )
-                compiled_baleen = optimizer.compile(CoTSimplifiedBaleen(), trainset=datasets[question_type]["train"][:50])
+                compiled_baleen = optimizer.compile(
+                    CoTSimplifiedBaleen(), trainset=datasets[question_type]["train"][:train_size]
+                )
             elif dspy_optimizer == "signature_optimizer":
                 optimizer = SignatureOptimizer(
-                    metric=evaluator.dspy_metric, breadth=10, depth=3, init_temperature=1.4, verbose=True, track_stats=True
+                    metric=evaluator.dspy_metric,
+                    breadth=10,
+                    depth=3,
+                    init_temperature=1.4,
+                    verbose=True,
+                    track_stats=True,
+                    prompt_model=teacher_lm,
                 )
                 eval_kwargs = dict(num_threads=1, display_progress=True, display_table=0)
                 compiled_baleen = optimizer.compile(
-                    CoTSimplifiedBaleen(), devset=datasets[question_type]["train"][:50], eval_kwargs=eval_kwargs
+                    CoTSimplifiedBaleen(),
+                    devset=datasets[question_type]["train"][:train_size],
+                    eval_kwargs=eval_kwargs,
                 )
             else:
                 raise Exception(f"Invalid dspy optimizer type: {dspy_optimizer}")
@@ -82,8 +100,23 @@ def main(dspy_method, dspy_optimizer):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run DSPY method.")
+
+    # dspy arguments
     parser.add_argument("dspy_method", type=str, help="The DSPY method to run")
     parser.add_argument("dspy_optimizer", type=str, help="The DSPY optimizer to use")
+    parser.add_argument("--student", default="gpt-3.5-turbo", type=str, help="The LLM to optimize prompts for")
+    parser.add_argument("--teacher", default=None, type=str, help="Teacher LLM for optimizing prompts. Defaults to Student LLM")
+    parser.add_argument("--train_size", default=50, type=int, help="Number of training examples to use for optimization")
+    parser.add_argument("--question_types", default=EVAL_QUESTION_TYPES, nargs="*", help="Question types. Defaults to all")
+
     args = parser.parse_args()
 
-    main(args.dspy_method, args.dspy_optimizer)
+    # setup LLMs
+    student_lm = dspy.OpenAI(model=args.student, max_tokens=1000)
+    teacher_lm = dspy.OpenAI(model=args.teacher, max_tokens=1000) if args.teacher else student_lm
+    dspy.settings.configure(lm=student_lm)
+
+    # validate question types
+    assert all([question_type in EVAL_QUESTION_TYPES for question_type in args.question_types])
+
+    main(args.dspy_method, args.dspy_optimizer, args.question_types, teacher_lm, args.train_size)
